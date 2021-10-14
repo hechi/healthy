d777a561ada1d7ca7c079432ca858027d6937ad7
Andreas Hechenberger <world@greenstation.de>
2021-10-14 09:47:11 +0200

    updates and fix version incompatibilities
    Several libraries got bumped by dependabot but those are partially incompatible with gradle and spring version. This commit will update and fix incompatibilities for kotlin.

--- healthy-api/build.gradle.kts
+++ healthy-api/build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.2.8.RELEASE"
-    id("io.spring.dependency-management") version "1.0.9.RELEASE"
-    kotlin("jvm") version "1.3.72"
-    kotlin("plugin.spring") version "1.3.72"
+    id("org.springframework.boot") version "2.5.5"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.5.31"
+    kotlin("plugin.spring") version "1.5.31"
 }
 
 group = "me.hechenberger"
