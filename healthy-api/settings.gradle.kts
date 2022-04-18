d7532e49f7eafeb368bbd9a3a04fffc1ca774e7a
Andy <world@greenstation.de>
2022-04-18 09:21:36 +0200

    updated backend libraries

--- healthy-api/settings.gradle.kts
+++ healthy-api/settings.gradle.kts
@@ -1,2 +1,7 @@
 rootProject.name = "healthy-api"
 
+// source https://github.com/jmfayard/refreshVersions
+plugins {
+    // See https://jmfayard.github.io/refreshVersions
+    id("de.fayard.refreshVersions") version "0.40.1"
+}
