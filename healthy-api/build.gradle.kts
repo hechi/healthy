a40eff49a545f93f151d170051503813de8cb5b5
Andy <world@greenstation.de>
2023-05-19 10:04:06 +0200

    fix jvm plugin version for kotlin

--- healthy-api/build.gradle.kts
+++ healthy-api/build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.7.10"
+    id("org.springframework.boot") version "2.7.12"
     id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "2.7.10"
-    kotlin("plugin.spring") version "2.7.10"
+    kotlin("jvm") version "1.8.21"
+    kotlin("plugin.spring") version "1.8.21"
 }
 
 group = "me.hechenberger"
